ef25dbc3828d35f792767ebab41ae2a379e97c7d
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-05-11 20:12:39 +0300

    working on errors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -64,7 +64,7 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 pt-[150px]">
+      className="w-full h-[100%] bg-[#0a192f] flex justify-center items-center p-4 pt-[150px]">
       <form
         className="flex flex-col max-w-[600px] w-full"
         onSubmit={submitHandler}
