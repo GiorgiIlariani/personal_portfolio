f3faad37c8ce770fdf540503c9df73a8558c2040
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-05-12 00:18:09 +0300

    errors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
           Giorgi Ilariani.
         </h1>
         <h2 className="text-[50px] sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Frontend Web developer from Georgia.
+          I'm a Frontend Web developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px] mt-2 text-xl">
           A Frontend focused Web Developer building the Frontend of Websites and
