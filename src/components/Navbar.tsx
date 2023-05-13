14eff09ad1431cc5a5a1f6fc67306e80de0e6835
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-05-13 21:25:36 +0300

    m

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -140,7 +140,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-103px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.facebook.com/"
+              href="https://github.com/GiorgiIlariani"
               target="_blank">
               Resume <BsFillPersonLinesFill size={30} />
             </a>
