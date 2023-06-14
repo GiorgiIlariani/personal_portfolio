2cd341a74d1d15ee94c7f202e0ae5d832eb8d2d1
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-06-15 00:18:19 +0300

    adds youtube_clone project

--- src/data/Work.ts
+++ src/data/Work.ts
@@ -8,6 +8,7 @@ import hooBank from '../assets/images/hooBank.png'
 import Covid_Questionary from '../assets/images/Covid_questionary.png'
 import Password_generator from '../assets/images/Password_generator.png'
 import carhub from '../assets/images/carhub.png'
+import youtube_clone from '../assets/images/youtube_clone.png'
 
 
 
@@ -22,64 +23,71 @@ export const data: ProjectItemProps[] = [
   github: "https://github.com/GiorgiIlariani/carhub",
   live: "https://carhub-alpha.vercel.app/",
   },
+    {
+  id: 2,
+  name: "youtube_clone",
+  image: youtube_clone,
+  github: "https://github.com/GiorgiIlariani/Youtube_clone",
+  live: "https://youtube-clone-psi-sandy.vercel.app/",
+  },
   {
-    id: 2,
+    id: 3,
     name: "Cv_Creator (React + Mui)",
     image: Cv_Creator,
     github: "https://github.com/GiorgiIlariani/Cv_Creator",
     live: "https://cv-creator-xi.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "Covid_Questionary(React + TS)",
     image: Covid_Questionary ,
     github: "https://github.com/GiorgiIlariani/Covid_Questionary",
     live: "https://covid-questionary.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "E-Commerce (React)",
     image: E_commerce,
     github: "https://github.com/GiorgiIlariani/E-commerce",
     live: "https://e-commerce-pi-eight.vercel.app/",
   },
     {
-    id: 5,
+    id: 6,
     name: "hooBank (React)",
     image: hooBank,
     github: "https://github.com/GiorgiIlariani/hooBank",
     live: "https://hoo-bank-rho.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Grocery_Bud (React)",
     image: Grocery_bud,
     github: "https://github.com/GiorgiIlariani/Grocery_Bud",
     live: "https://grocery-bud-kappa.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     name: "Tours(React)",
     image: Tours,
     github: "https://github.com/GiorgiIlariani/Tours",
     live: "https://tours-eta-liard.vercel.app/",
   },
   {
-    id: 8,
+    id: 9,
     name: "Weather (JS)",
     image: Weather_api,
     github: "https://github.com/GiorgiIlariani/weather_api",
     live: "https://weather-api-delta-ebon.vercel.app/",
   },
   {
-    id: 9,
+    id: 10,
     name: "Grocery_Bud (JS)",
     image: Grocery_bud_js,
     github: "https://github.com/GiorgiIlariani/grocery_bud_js",
     live: "https://grocery-bud-js.vercel.app/",
   },
   {
-    id: 10,
+    id: 11,
     name: "Password generator (JS)",
     image: Password_generator,
     github: "https://github.com/GiorgiIlariani/password_generator",
